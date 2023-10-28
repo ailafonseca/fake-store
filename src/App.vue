9432253eadb0f2b08c436c319c004ac7b52d9975
aila <ailafonseca16@gmail.com>
2023-10-28 01:21:14 -0300

    responsive design + card layout improvement

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ProductCard from './components/ProductCard.vue'
+</script>
+
+<template>
+  <div class="bg-primary h-auto p-2 flex items-center justify-center">
+    <h1
+      class="font-mono text-4xl text-white underline decoration-indigo-300 decoration-wavy underline-offset-4"
+    >
+      Store
+    </h1>
+  </div>
+  <ProductCard />
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: ["light"]
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")]
+}
+  
