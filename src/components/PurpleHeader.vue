6bc61c4e7d675192a30399afd14a60ab48dbf90b
aila <ailafonseca16@gmail.com>
2024-06-15 12:56:33 -0300

    logo and store name redirecting to home view page

--- src/components/PurpleHeader.vue
+++ src/components/PurpleHeader.vue
@@ -10,20 +10,25 @@ const cartStore = useCartStore()
     class="font-mono text-purple-900 bg-purple-300 p-2 md:flex items-center md:justify-between grid grid-cols-2 w-auto"
   >
     <div class="flex items-center justify-center md:ml-12">
-      <img src="/./src/assets/store-shopping-ecommerce-svgrepo-com.svg" class="h-14 w-14" />
-      <div class="md:text-2xl text-xl">Store</div>
+      <RouterLink to="/">
+        <img src="/./src/assets/store-shopping-ecommerce-svgrepo-com.svg" class="h-14 w-14" />
+      </RouterLink>
+
+      <div class="md:text-2xl text-xl">
+        <RouterLink to="/"> Store</RouterLink>
+      </div>
     </div>
     <div class="flex items-center justify-center md:mr-12">
       <div class="md:grid-flow-col grid grid-cols-1 md:text-base md:ml-4 md:space-x-4 mt-3 mr-4">
         <RouterLink to="/">Products</RouterLink>
         <RouterLink to="/purchases">Purchases</RouterLink>
       </div>
-      <label for="cart" class="drawer-button btn btn-ghost btn-xs w-8 p-0">
+      <div @click="cartStore.drawerToggle" class="relative cursor-pointer">
         <ShoppingCartIcon class="w-6" />
-        <div class="badge badge-ghost md:badge-sm badge-xs relative -top-9 -right-3">
+        <div class="badge badge-ghost md:badge-sm badge-xs absolute -top-2 -right-3">
           {{ cartStore.calculateTotalProductsQuantity }}
         </div>
-      </label>
+      </div>
     </div>
   </div>
 </template>
